94d2d3b99e86785b653b5d39887c72fa4364b19c
Kosin89 <110737422+Kosin89@users.noreply.github.com>
2023-01-07 18:15:35 +0100

    drink search

--- Wyszukiwarka/js/main.js
+++ Wyszukiwarka/js/main.js
@@ -1,3 +1,25 @@
+// <!-- 
+// Wskazówki
+
+//     Zastanów się jakich elementów będziesz potrzebował/a (jakie elementy będziesz musiał/a pobrać za pomocą np. querySelectora).
+//     Stwórz funkcję, która będzie się odpalała, kiedy wpiszemy coś do inputa.
+//     Funkcja ma porównywać to, co wpisaliśmy w inputa z każdym napojem na liście.
+//     Za pomocą instrukcji warunkowej sprawdź, czy dane z inputa pokrywają się z nazwą napoju. Jeśli nie, ukryj taki napój.
+//     Żeby aplikacja działała poprawnie, musimy się zabezpieczyć. Użytkownik może podać nazwę napoju dużymi lub małymi literami. Musimy więc sprowadzić to, co wpisuje użytkownik do np. małych liter (tak samo trzeba zrobić z listą napojów).
+
+
+// Podpowiedzi do wskazówek
+
+//     Trzeba będzie stworzyć 2 zmienne, które będą przechowywały inputa oraz listę napojów (wszystkie <li>).
+//     addEventListener na event 'keyup'.
+//     Chcemy przeszukać całą listę napojów i na każdym z nich wykonać dokładnie tę samą funkcję. Brzmi jak zadanie dla pętli, prawda? ;)
+//     Warto też zauważyć, że skoro pobraliśmy wszystkie <li> do jednej zmiennej, to mamy obiekt tablico podobny – idealnie! Pętle z tablicami się przecież bardzo lubią. :)
+//     Co więcej, mamy taką metodę (coś z indeksem w nazwie), która sprawdza, czy jakiś znak / jakieś znaki są w danym stringu. Jeśli nie ma, to zwraca wartość -1.
+//     Jeśli coś zwróci -1 to taki napój należy ukryć. display: none; robi robotę. ;)
+//     Mamy takie dwie fajne metody – toUpperCase lub toLowerCase. :) -->
+
+
+
 const search = document.querySelector(".search")
 const li = document.querySelectorAll("li")
 
@@ -5,11 +27,8 @@ const searchEngine = e => {
 	const text = e.target.value.toLowerCase()
 	
 
-   li.forEach(el => {
-
-      const task = el.textContent;
-
-      if (task.toLowerCase().indexOf(text) !== -1) {
+   li.forEach(el => {     
+      if (el.textContent.toLowerCase().indexOf(text) !== -1) {
          el.style.display = 'block'
       } else {
          el.style.display = 'none'
@@ -19,3 +38,11 @@ const searchEngine = e => {
 }
 
 search.addEventListener('keyup', searchEngine)
+
+
+
+
+// 1. Listener nasluchuje na zmiany w Inpucie
+// 2. e.target wskazuje na input .e.target.value wskazuje na wartość inputa, cyzli TextTrackList, który wpisujemy.
+// 3. Zmienna li przechowuje wszystkie napoje.
+// 4. el odnosi sie do kazdego li na liście.
